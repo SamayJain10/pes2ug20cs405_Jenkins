d188f1afaeff98bac39f1194d04da3f1fc9dcc15
Samay Jain <92438232+BinaryBeast10@users.noreply.github.com>
2023-02-16 14:09:11 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, Earthlings\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
